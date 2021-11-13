007c3f5fa0527248a2988047f4baa3aacb68e116
Gabriel Ghisleni <gabriele.ghisleni01@gmail.com>
2021-11-13 12:20:44 +0100

    ADDED JSON BIN

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,27 +2,35 @@ import React, {  Component } from 'react'
 import { Container }from 'reactstrap'
 
 import * as data from './shared/data.json';
+import * as axios from 'axios'
 import CustomForm from './CustomForm'
 import shuffle_data from './shuffle';
 import Header from "./Header";
 import Footer from './Footer'; 
+import { SETTINGS } from './shared/settings';
 
 class  Main extends Component {
     constructor(props) {
         super(props);
-        this.state = { lang:'it' }
+        this.state = { lang:'it', previous:null }
         this.changeLang = this.changeLang.bind(this)}
-        
-    changeLang(val){this.setState({lang:val})}
 
+    changeLang(val){this.setState({lang:val})}
+    componentDidMount(){
+        axios.get(SETTINGS.jsonbin+"latest")
+        .then(res => this.setState({previous: res.data}))
+        .catch(e => {alert("JSON BIN WRONG")})
+    }
+    
     render() { 
+        console.log(this.state.previous)
         const DATA = data.default
         const SHUFFLE_DATA = shuffle_data(DATA)
         return ( 
         <React.Fragment>
             <Container className='main-container'>
             <Header lang={this.state.lang} langF={this.changeLang}/>
-            <CustomForm lang={this.state.lang} data={SHUFFLE_DATA}/>
+            <CustomForm lang={this.state.lang} data={SHUFFLE_DATA} previous={this.state.previous}/>
             </Container>
             <Footer />
         </React.Fragment>  );
